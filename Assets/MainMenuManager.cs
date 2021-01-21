9d62e6b60ebabbab0eac3dd07a411924afcd31d4
Nicolas "Nic" Gard <nicolasg699@gmail.com>
2021-01-21 12:18:24 -0600

    main menu shows current version number

---
+++ Assets/MainMenuManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] GameObject versionNumberText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        versionNumberText.GetComponent<TMPro.TextMeshProUGUI>().text = "v. " + GlobalConfig.major_version + "." + GlobalConfig.minor_version;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
