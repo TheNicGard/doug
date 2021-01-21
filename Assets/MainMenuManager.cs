bc532254abb3eb950f843d34d9867392bd4c3502
Nicolas "Nic" Gard <nicolasg699@gmail.com>
2021-01-21 12:46:12 -0600

    added a version notice to main menu

--- Assets/MainMenuManager.cs
+++ Assets/MainMenuManager.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
-    [SerializeField] GameObject versionNumberText;
+    [SerializeField] GameObject versionNumberText = null;
+    [SerializeField] GameObject versionNoticeText = null;
 
     // Start is called before the first frame update
     void Start()
@@ -12,9 +13,16 @@ public class MainMenuManager : MonoBehaviour
         versionNumberText.GetComponent<TMPro.TextMeshProUGUI>().text = "v. " + GlobalConfig.major_version + "." + GlobalConfig.minor_version;
     }
 
+    bool instantiated = false;
     // Update is called once per frame
     void Update()
     {
-        
+        if (!instantiated && PersistentGameManager.instance != null)
+        {
+            if (GlobalConfig.major_version != PersistentGameManager.instance.playerData.playerData.savefileVersionMajor &&
+            GlobalConfig.minor_version != PersistentGameManager.instance.playerData.playerData.savefileVersionMinor)
+            versionNoticeText.SetActive(true);
+            instantiated = true;
+        }
     }
 }
